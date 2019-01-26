211791fef770e1e5a2d48d30b4c6deb4663645dc
zhangaoxiang <iszhangax@gmail.com>
2019-01-27 01:26:07 +0800

    1

--- 2012/3another.cpp
+++ 2012/3another.cpp
@@ -1,4 +1,4 @@
-// 传指针要提前分配空间，指针是传值不是传引用
+// 传引用做法
 
 #include<iostream>
 #include<algorithm>
@@ -11,20 +11,20 @@ struct node{
     char name;
 };
 
-void build(string post, string in, node *tree) {
+void build(string post, string in, node &tree) {
     int i;
     string a, b;
-    tree->name = post[post.length() - 1];
+    tree.name = post[post.length() - 1];
     for(i = 0; i < in.length(); i++)
         if(in[i] == post[post.length() - 1])
             break;
     if(i > 0) {
-        tree->lnode = new node;
-        build(post.substr(0, i), in.substr(0, i), tree->lnode);
+        tree.lnode = new node;
+        build(post.substr(0, i), in.substr(0, i), *tree.lnode);
     }
     if(i < post.length() - 1) {
-        tree->rnode = new node;
-        build(post.substr(i, post.length() - i - 1), in.substr(i + 1), tree->rnode);
+        tree.rnode = new node;
+        build(post.substr(i, post.length() - i - 1), in.substr(i + 1), *tree.rnode);
     }
         
 }
@@ -40,8 +40,8 @@ void preorder(node *tree) {
 int main() {
     string postorder, inorder;
     cin >> postorder >> inorder;
-    node *tree = new node;
+    node tree;
     build(postorder, inorder, tree);
-    preorder(tree);
+    preorder(&tree);
     return 0;
 }
