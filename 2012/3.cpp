bf26072343a53458e5f3b0576cf23ed3c1a0a24c
zhangaoxiang <iszhangax@gmail.com>
2019-01-27 00:53:27 +0800

    1

---
+++ 2012/3.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+struct node{
+    node *lnode = NULL;
+    node *rnode = NULL;
+    char name;
+};
+
+void build(string post, string in, node *tree) {
+    int i;
+    string a, b;
+    tree->name = post[post.length() - 1];
+    cout << tree->name << endl;
+    for(i = 0; i < in.length(); i++)
+        if(in[i] == post[post.length() - 1])
+            break;
+    if(i > 0) {
+        tree->lnode = new node;
+        build(post.substr(0, i), in.substr(0, i), tree->lnode);
+    }
+    if(i < post.length() - 1) {
+        tree->rnode = new node;
+        build(post.substr(i, post.length() - i - 1), in.substr(i + 1), tree->rnode);
+    }
+        
+}
+
+void preorder(node *tree) {
+    if(tree == NULL)
+        return;
+    cout << tree->name;
+    preorder(tree->lnode);
+    preorder(tree->rnode);
+}
+
+int main() {
+    string postorder, inorder;
+    cin >> postorder >> inorder;
+    node *tree = new node;
+    build(postorder, inorder, tree);
+    preorder(tree);
+    return 0;
+}
